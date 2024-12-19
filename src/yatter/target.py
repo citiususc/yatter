b69f3c5f244d642c3d140d1b6ef18e1bf9a3423f
bhorinrb <bujanvideruben@gmail.com>
2024-12-19 12:34:54 +0100

    Simplified code

--- src/yatter/target.py
+++ src/yatter/target.py
@@ -4,7 +4,7 @@ from .constants import *
 def add_logical_targets(yarrrml_data, added_targets):
     logical_targets = []
     for target in added_targets:
-        logical_targets.extend(generate_added_logical_target(added_targets.get(target), target))
+        logical_targets.extend(generate_logical_target(added_targets.get(target), target))
     for mapping in yarrrml_data.get(YARRRML_MAPPINGS):
         mapping_data = yarrrml_data.get(YARRRML_MAPPINGS).get(mapping)
         add_internal_logical_target(mapping, mapping_data, logical_targets, added_targets)
@@ -35,9 +35,9 @@ def add_internal_logical_target(mapping_id, mapping_data, internal_targets, adde
                     if type(value) is list:
                         for v in value:
                             if type(v) is dict:
-                                add_internal_logical_target(mapping_id, v, internal_targets, added_targets,local_target_id)
+                                add_internal_logical_target(mapping_id, v, internal_targets, added_targets, local_target_id)
                     elif type(value) is dict:
-                        add_internal_logical_target(mapping_id, value, internal_targets, added_targets,local_target_id)
+                        add_internal_logical_target(mapping_id, value, internal_targets, added_targets, local_target_id)
         elif YARRRML_TARGETS in mapping_data[key]:
             target_value = mapping_data[key][YARRRML_TARGETS]
             logical_target_id = "logical_target_" + mapping_id + "_" + str(local_target_id)
@@ -47,47 +47,6 @@ def add_internal_logical_target(mapping_id, mapping_data, internal_targets, adde
 
 
 def generate_logical_target(target_yaml_data, id_target):
-    logical_targets = []
-    output_type = None
-    format = None
-    compression = None
-    target_yaml_data_list = target_yaml_data
-    if isinstance(target_yaml_data_list, dict):
-        target_yaml_data_list = [target_yaml_data]
-    for target_yaml_data in target_yaml_data_list:
-        logical_target = ["<" + id_target + "> a " + RML_LOGICAL_TARGET_CLASS + ";\n"]
-        if YARRRML_ACCESS not in target_yaml_data:
-            _, target_yaml_data = target_yaml_data.popitem()
-
-        access = target_yaml_data[YARRRML_ACCESS]
-        if YARRRML_TYPE in target_yaml_data:
-            output_type = target_yaml_data[YARRRML_TYPE]
-        if YARRRML_SERIALIZATION in target_yaml_data:
-            format = "formats:" + YARRRML_OUTPUT_FORMAT[target_yaml_data[YARRRML_SERIALIZATION]]
-        if YARRRML_COMPRESSION in target_yaml_data:
-            compression = "comp:" + target_yaml_data[YARRRML_COMPRESSION]
-
-        logical_target.append("\t " + RML_TARGET + " [\n\t\t")
-
-        if output_type == "sparql":
-            logical_target.append("sd:endpoint <" + access + ">;\n\t\tsd:supportedLanguage sd:SPARQL11Update\n\t];\n")
-        elif output_type == "dcat":
-            logical_target.append("a dcat:Dataset;\n\t\tdcat:dataDump <" + access + ">\n\t];\n")
-        else:
-            logical_target.append("a void:Dataset;\n\t\tvoid:dataDump <" + access + ">\n\t];\n")
-
-        if format:
-            logical_target.append("\t " + RML_SERIALIZATION + " " + format + ";")
-        else:
-            logical_target.append("\t " + RML_SERIALIZATION + " formats:N-Quads;")
-
-        if compression:
-            logical_target.append("\n\t " + RML_COMPRESSION + " " + compression + ";")
-        logical_targets.append("".join(logical_target)[0:-1] + ".\n\n")
-    return logical_targets
-
-
-def generate_added_logical_target(target_yaml_data, id_target):
     logical_targets = []
     output_type = None
     format = None
